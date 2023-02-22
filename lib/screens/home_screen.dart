b16522550adf91154513beed26bc4159544c9a1d
dennis-adrian <dennisguzmanbo@gmail.com>
2023-02-21 20:34:10 -0400

    Add floatingActionButton

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,20 +7,23 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     const fontSize24 = TextStyle(fontSize: 24);
     return Scaffold(
-        appBar: AppBar(title: const Text('Home Screen')),
-        body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
-                  'Number of clicks',
-                  style: fontSize24,
-                ),
-                Text(
-                  '10',
-                  style: fontSize24,
-                )
-              ]),
-        ));
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                'Number of clicks',
+                style: fontSize24,
+              ),
+              Text(
+                '10',
+                style: fontSize24,
+              )
+            ]),
+      ),
+      floatingActionButton:
+          FloatingActionButton(child: const Icon(Icons.add), onPressed: () {}),
+    );
   }
 }
